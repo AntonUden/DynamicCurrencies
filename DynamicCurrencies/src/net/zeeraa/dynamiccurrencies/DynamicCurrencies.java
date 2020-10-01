12e94008af1048d4b2e01119c06f5c2335d0fa13
Anton udén <udenanton@hotmail.com>
2020-10-01 16:57:42 +0200

    Setup APIImplementation on enable

--- DynamicCurrencies/src/net/zeeraa/dynamiccurrencies/DynamicCurrencies.java
+++ DynamicCurrencies/src/net/zeeraa/dynamiccurrencies/DynamicCurrencies.java
@@ -4,8 +4,15 @@ import java.io.File;
 
 import org.bukkit.configuration.serialization.ConfigurationSerialization;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.FileUtil;
 
+import net.zeeraa.dynamiccurrencies.api.APIImplementation;
+import net.zeeraa.dynamiccurrencies.api.DynamicCurrenciesAPI;
 import net.zeeraa.dynamiccurrencies.api.account.Account;
+import net.zeeraa.dynamiccurrencies.api.currency.Currency;
+import net.zeeraa.dynamiccurrencies.api.currency.CurrencyDataManager;
+import net.zeeraa.dynamiccurrencies.datamanagers.DefaultCurrencyDataManager;
+import net.zeeraa.dynamiccurrencies.datamanagers.DefaultPlayerDataManager;
 
 
 public class DynamicCurrencies extends JavaPlugin {
@@ -36,6 +43,13 @@ public class DynamicCurrencies extends JavaPlugin {
 
 		playerDataFolder = getFileFromPlaceholders(getConfig().getString("player-data-folder"));
 		currenciesFile = getFileFromPlaceholders(getConfig().getString("currencies-file"));
+		
+	
+		
+		APIImplementation.setCurrencyDataManager(new DefaultCurrencyDataManager());
+		APIImplementation.setPlayerDataManager(new DefaultPlayerDataManager());
+		
+		DynamicCurrenciesAPI.getCurrencyDataManager().loadCurrencies();
 	}
 
 	private File getFileFromPlaceholders(String path) {
